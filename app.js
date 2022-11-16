c5e926e975d62e31340c3d2ea4539332976e3d3a
mehul75672 <vaniyamehul2692000@gmail.com>
2022-11-16 16:58:53 +0530

    first

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public/images')));
 
+
+app.get("/", (req, res) => {
+  res.status(200).send("welcome to home page");
+})
+
 //all routes
 const admin_routes = require("./routes/admin");
 const user_routes = require("./routes/user");
@@ -25,18 +30,10 @@ app.use(admin_routes);
 app.use(user_routes);
 require('./seeder/admin')
 
-app.get("/", (req, res) => {
-  res.status(200).send("welcome to home page");
-})
-
 
 const PORT = process.env.port;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`)
-});
-
-app.get('/', (req, res) => {
-  res.send("<button><a href='/user'>Login With Google</a></button>")
 });
 
 
